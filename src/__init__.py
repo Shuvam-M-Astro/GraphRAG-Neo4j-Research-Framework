c71b656e04816bd9306b0adaaac51e3dffb06cf7
Shuvam Mondal <96789016+Shuvam-M-Astro@users.noreply.github.com>
2025-07-22 11:00:28 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,9 @@
-# Graph RAG Scientific Research Package 
+"""
+GraphRAG Neo4j Research Framework - Main Package
+"""
+
+from .validation import *
+from .types import *
+
+__version__ = "1.0.0"
+__author__ = "GraphRAG Neo4j Research Framework Team" 
